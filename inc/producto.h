ed307a2a80664179c1ac36d392bc01fc7cc7717b
BGPS1996 <grabixz.29@gmail.com>
2025-11-25 23:04:03 -0300

    add fila class

--- inc/producto.h
+++ inc/producto.h
@@ -12,7 +12,8 @@ private:
     string vencimiento_;        	//formato : "YYYY-MM-DD"
     string tipo_almacenamiento_; 	//p.ej. "Refrigerado", "Seco", "Congelado"
     string clasificacion_;          // notas/etiquetas especiales (p.ej. "Sin TACC", "Orgánico")
-
+    // Agregar volumen.
+    // Agregar get_volumen();
 public:
     // --- Constructores ---
     Producto() = default;
